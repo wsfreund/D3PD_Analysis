4ba419556c602923ea301c72175ebee2c3bf994a
Werner Freund <wsfreund@gmail.com>
2012-11-10 00:39:41 -0200

    Corrigido outro pequeno problema

--- MatlabD3PDImport/inc/d3pd_export.m
+++ MatlabD3PDImport/inc/d3pd_export.m
@@ -9,16 +9,16 @@ pyFile=fopen(fileName,'a');
 
 fprintf(pyFile,'self.sgn_cl_size=%d\n',analysis.sgn_cl_size);
 fprintf(pyFile,'self.bkg_cl_size=%d\n',analysis.bkg_cl_size);
-fprintf(pyFile,'self.sgn_tst_clusters=[')
+fprintf(pyFile,'self.sgn_tst_clusters=[');
 for k=1:length(analysis.(hNNname).(nNetTrnInfo).sgn_tst_clusters)-1
   fprintf(pyFile,'%d,',analysis.(hNNname).(nNetTrnInfo).sgn_tst_clusters(k));
 end
-fprintf(pyFile,'%d]\n',analysis.(hNNname).(nNetTrnInfo).sgn_tst_clusters(end))
-fprintf(pyFile,'self.bkg_tst_clusters=[')
+fprintf(pyFile,'%d]\n',analysis.(hNNname).(nNetTrnInfo).sgn_tst_clusters(end));
+fprintf(pyFile,'self.bkg_tst_clusters=[');
 for k=1:length(analysis.(hNNname).(nNetTrnInfo).bkg_tst_clusters)-1
-  fprintf(pyFile,'%d ',analysis.(hNNname).(nNetTrnInfo).bkg_tst_clusters(k));
+  fprintf(pyFile,'%d,',analysis.(hNNname).(nNetTrnInfo).bkg_tst_clusters(k));
 end
-fprintf(pyFile,'%d]\n',analysis.(hNNname).(nNetTrnInfo).bkg_tst_clusters(end))
-fprintf(pyFile, 'self.ringerNNTrnWrt=%d',1); % TODO Change it to read an variable of analysis
-fprintf(pyFile, 'self.sgnTrnPdgIdType=%d',11); % TODO Change it to read an variable of analysis
-fprintf(pyFile, 'self.sgnTrnMotherPdgIdType=%d',22); % TODO Change it to read an variable of analysis
+fprintf(pyFile,'%d]\n',analysis.(hNNname).(nNetTrnInfo).bkg_tst_clusters(end));
+fprintf(pyFile, 'self.ringerNNTrnWrt=%d\n',1); % TODO Change it to read an variable of analysis
+fprintf(pyFile, 'self.sgnTrnPdgIdType=%d\n',11); % TODO Change it to read an variable of analysis
+fprintf(pyFile, 'self.sgnTrnMotherPdgIdType=%d\n',22); % TODO Change it to read an variable of analysis
