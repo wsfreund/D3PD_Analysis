e26faba05668e0bf06916134b14ffd48eafffdce
Werner Freund <wsfreund@gmail.com>
2014-10-01 19:30:21 -0300

    Improved stochastic implementation

--- MatlabRingerTrain/matlab/MatchedFilterPropagate.m
+++ MatlabRingerTrain/matlab/MatchedFilterPropagate.m
@@ -39,23 +39,36 @@ function filterAns = MatchedFilterPropagate(matchedFilterStruct,...
   case 'deterministic'
     filterAns = Id;
   case 'stochastic'
-    N0 = matchedFilterStruct.opts.N0;
+    % Facilitate access to some variables:
+    N0 = opts.N0;
+    matchedFilterStruct.nCompH0d = 100;
+    matchedFilterStruct.nCompH1d = 100;
+    compArray0 = 1:matchedFilterStruct.nCompH0d;
+    compArray1 = 1:matchedFilterStruct.nCompH1d;
     % Ir : for Ir we work on the sample without the mean, since we
     % removed it for the pca calculation:
-    keyboard
+    if opts.scaleNoise
+      scaledNoise0 = N0*nCompH0d/...
+        (numel(matchedFilterStruct.lambda0square)*2);
+      scaledNoise1 = N0*nCompH1d/...
+        (numel(matchedFilterStruct.lambda1square)*2);
+    else
+      scaledNoise0 = N0/2;
+      scaledNoise1 = N0/2;
+    end
     Ir = ...
       ... % Ir0:
-      bsxfun(@times,... % This multiplication is the scale factor
-        matchedFilterStruct.phi0(1:matchedFilterStruct.nCompH0d,:)* ... % Project data
-        bsxfun(@minus,inData,matchedFilterStruct.dataMean0).^2, ... % Take square from the projected data
-        (matchedFilterStruct.lambda0square ./ ...    % And here is the scale factor
-        (matchedFilterStruct.lambda0square+N0/2))) ...
-      - ... % Ir1:
-      bsxfun(@times,... % This multiplication is the scale factor
-        matchedFilterStruct.phi1(1:matchedFilterStruct.nCompH1d,:)* ... % Project data
-        bsxfun(@minus,inData,matchedFilterStruct.dataMean1).^2, ... % Take square from the projected data
-        (matchedFilterStruct.lambda1square./...    % And here is the scale factor
-        (matchedFilterStruct.lambda1square+N0/2)));
+      (...
+        (matchedFilterStruct.lambda0square(compArray0) ./ ...  % Scale factor
+        (matchedFilterStruct.lambda0square(compArray0)+scaledNoise0))* ...
+        (matchedFilterStruct.phi0(compArray0,:)* ... % Project data
+        bsxfun(@minus,inData,matchedFilterStruct.dataMean0)).^2 ... % Take square from the projected data
+        - ... % Ir1:
+        (matchedFilterStruct.lambda1square(compArray1)./...    % Scale factor
+        (matchedFilterStruct.lambda1square(compArray1)+scaledNoise1))* ...
+        (matchedFilterStruct.phi1(compArray1,:)* ... % Project data
+        bsxfun(@minus,inData,matchedFilterStruct.dataMean1)).^2 ... % Take square from the projected data
+      )/N0; % And normalize everything by N0
     % Filter answer is the sum from the deterministic component and 
     filterAns = Ir + Id; 
   end
